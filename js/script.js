070edafb306a48343592646c3fc3826076594af8
fahdfady <55513802+fahdfady@users.noreply.github.com>
2021-01-16 19:52:26 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -60,10 +60,10 @@ var swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
     loop: true,
     spaceBetween: 30,
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
