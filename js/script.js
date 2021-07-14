b2fc48bae935bf1f5a801d87e421261675dfeacd
fahdfady <55513802+fahdfady@users.noreply.github.com>
2021-07-14 08:10:32 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// loader
+
+
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		// $('h1').css('color','#222222');
+	}, 3000);
+	
+});
+
+6
+
 function smoothScroll() {
 
 
